dccf2c7e27c04299b0292754c4092f6733d7513c
boodscode237 <boodscode@gmail.com>
2022-05-12 16:39:13 +0500

    Started adding and deleting items from the server

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,18 +1,26 @@
 import React from "react";
 import {Card, CardContent, CardHeader, CardMedia, IconButton} from "@mui/material";
 import {DeleteOutlined} from "@mui/icons-material";
+import Typography from "@material-ui/core/Typography";
 
-export default function NoteCard({note}) {
+export default function NoteCard({note, handleDelete}) {
     return(
         <div>
-            <Card>
+            <Card elevation={3}>
                 <CardHeader
                     action={
-                    <IconButton>
+                    <IconButton onClick={() => handleDelete(note.id)}>
                         <DeleteOutlined/>
                     </IconButton>
                     }
+                    title={note.title}
+                    subheader={note.category}
                 />
+                <CardContent>
+                    <Typography variant={"body2"} color={"textSecondary"}>
+                        {note.details}
+                    </Typography>
+                </CardContent>
             </Card>
         </div>
     )
--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -11,13 +11,22 @@ useEffect(() => {
         .then(data => setNotes((data)))
 
 }, [])
+
+    const handleDelete = async (id) => {
+        await  fetch("http://localhost:8000/notes" + id, {
+            method: 'DELETE'
+        })
+
+        const newNotes = notes.filter(note => note.id != id)
+        setNotes(newNotes)
+    }
   return (
     <Container>
-        <Grid container>
+        <Grid container spacing={3}>
             {notes.map(note => (
                 <Grid  key={note.id} item xs={12} sm={6} md={3}>
                     <Paper>{ note.title }</Paper>
-                    <NoteCard note={note}/>
+                    <NoteCard note={note} handleDelete={handleDelete()}/>
                 </Grid>
             ))}
         </Grid>
